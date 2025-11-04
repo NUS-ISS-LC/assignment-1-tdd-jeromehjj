6bfc98f699acf5517567b17dfb55560166911f12
jeromehjj <97333019+jeromehjj@users.noreply.github.com>
2025-11-04 03:01:11 +0000

    Implement test case methods and todolist methods

--- src/main/java/sg/edu/nus/iss/epat/tdd/workshop/ToDoList.java
+++ src/main/java/sg/edu/nus/iss/epat/tdd/workshop/ToDoList.java
@@ -1,31 +1,36 @@
 package sg.edu.nus.iss.epat.tdd.workshop;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.stream.Collectors;
 
 public class ToDoList {
    private HashMap<String, Task> tasks = new HashMap<String, Task>();
 
    public void addTask(Task task) {
       // Add code here
+      tasks.put(task.getDescription(), task);
    }
 
    public void completeTask(String description) {
       // Add code here
+      Task task = tasks.get(description);
+      task.setComplete(true);
    }
 
    public boolean getStatus(String description) {
       // Add code here
-      return false;
+      Task task = tasks.get(description);
+      return task != null && task.isComplete();
    }
 
    public Task getTask(String description) {
       // Add code here
-      return null;
+      return tasks.get(description);
    }
 
    public Task removeTask(String description) {
       // Add code here
-      return null;
+      return tasks.remove(description);
    }
 
    public Collection<Task> getAllTasks() {
@@ -34,6 +39,8 @@ public class ToDoList {
 
    public Collection<Task> getCompletedTasks() {
       // Add code here
-      return null;
+      return tasks.values().stream()
+                .filter(Task::isComplete)
+                .collect(Collectors.toList());
    }
 }
--- src/test/java/sg/edu/nus/iss/epat/tdd/workshop/ToDoListTest.java
+++ src/test/java/sg/edu/nus/iss/epat/tdd/workshop/ToDoListTest.java
@@ -1,13 +1,18 @@
 package sg.edu.nus.iss.epat.tdd.workshop;
 
+import java.util.stream.Collectors;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 import static org.junit.Assert.fail;
 
 public class ToDoListTest  {
     // Define Test Fixtures
+    private ToDoList toDoList;
+    private Task t1, t2;
 
     public ToDoListTest() {
         super();
@@ -16,30 +21,61 @@ public class ToDoListTest  {
     @Before
     public void setUp() throws Exception {
         // Initialise Test Fixtures
+        t1 = new Task("Test 1", false);
+        t2 = new Task("Test 2", false);
+        toDoList = new ToDoList();
     }
 
     @After
     public void tearDown() throws Exception {
         // Uninitialise test Fixtures
+        t1 = null;
+        t2 = null;
+        toDoList = null;
     }
 
     @Test
     public void testAddTask() {
-        fail("Not implemented yet");
+        toDoList.addTask(t1);
+
+        // Test that the task list is not empty
+        assertFalse(toDoList.getAllTasks().isEmpty());
+
+        // Test that the task in list is same as t1
+        assertEquals(toDoList.getTask(t1.getDescription()), t1);
     }
 
     @Test
     public void testGetStatus() {
-        fail("Not implemented yet");
+        toDoList.addTask(t1);
+        toDoList.addTask(t2);
+
+        // Test the status of t1
+        assertEquals(t1.isComplete(), toDoList.getStatus(t1.getDescription()));
+
+        // Test if the status of t2 is indeed false
+        assertFalse(toDoList.getStatus(t2.getDescription()));
     }
 
     @Test
     public void testRemoveTask() {
-        fail("Not implemented yet");
+        toDoList.addTask(t2);
+        toDoList.removeTask(t2.getDescription());
+
+        // Test that the task list is indeed empty after removing
+        assertTrue(toDoList.getAllTasks().isEmpty());
     }
 
     @Test
     public void testGetCompletedTasks() {
-        fail("Not implemented yet");
+        toDoList.addTask(t1);
+        toDoList.addTask(t2);
+        toDoList.completeTask("Test 2");
+
+        // Test that number of completed tasks is 1
+        assertEquals(1, toDoList.getCompletedTasks().size());
+
+        // Test that the completed task is 2
+        assertEquals(t2, toDoList.getCompletedTasks().iterator().next());
     }
 }
